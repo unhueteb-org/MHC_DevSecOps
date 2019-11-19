a9f39689e0c55479bea22a2e393c4e186040d485
Roberto Borges <robertobborges@gmail.com>
2019-11-18 22:06:58 -0500

    Added OWASP top 10

--- IaC-AzureEnvCreation.ps1
+++ IaC-AzureEnvCreation.ps1
@@ -2,25 +2,26 @@
 $version=(az aks get-versions -l eastus --query 'orchestrators[-1].orchestratorVersion' -o tsv)
 $unicstr = -join ((97..122) | Get-Random -Count 5 | % {[char]$_})
 $aksname = "devsecops"+ $unicstr + "aks" 
-$rgname = "akshandsonlab-" + $unicstr + "-rg"
+$rgname = "DevSecOps-" + $unicstr + "-RG"
 $acrname = "devsecops" + $unicstr + "acr"
 $sqlsvname =  "devsecops" + $unicstr + "db"
+$appServicePlan = "owasp" + $unicstr + "t10"
+$app = "owaspapp" + $unicstr + "t10"
 
 # Register the network provider
 az provider register --namespace Microsoft.Network
 
 # Create a ressource groupe
-az group create --name akshandsonlab-$unicstr-rg --location eastus
-Write-Host 'Ressource groupe : $($rgname) created '
+az group create --name $rgname --location eastus
+Write-Host 'Ressource groupe : ' + $acrname + ' created '
 
 # Create AKS Service
-#az aks create --resource-group $rgname --name $aksname --enable-addons monitoring --kubernetes-version $version --generate-ssh-keys --location eastus
 az aks create --resource-group $rgname --name $aksname --enable-addons monitoring --kubernetes-version 1.12.7 --generate-ssh-keys --location eastus
-Write-Host 'Azure Kubernetes Service : $($aksname) created '
+Write-Host 'Azure Kubernetes Service : ' + $acrname + ' created '
 
 # Create ACR service
 az acr create --resource-group $rgname --name $acrname --sku Standard --location eastus
-Write-Host 'Azure Container Registry : $($acrname) created '
+Write-Host 'Azure Container Registry : ' + $acrname + ' created '
 
 # Get the id of the service principal configured for AKS
 $CLIENT_ID=(az aks show --resource-group $rgname --name $aksname --query "servicePrincipalProfile.clientId" --output tsv)
@@ -36,9 +37,15 @@ az sql server create -l eastus -g $rgname -n $sqlsvname -u sqladmin -p P2ssw0rd1
 
 az sql db create -g $rgname -s $sqlsvname -n mhcdb --service-objective S0
 
+# Create service plan for OWASP top 10
+az appservice plan create -g $rgname -n $appServicePlan --is-linux --number-of-workers 1 --sku B1
+
+# Create service WebApp for OWASP top 10
+az webapp create --resource-group $rgname --plan $appServicePlan --name $app --deployment-container-image-name bkimminich/juice-shop
+
 Write-Host "======================================================================================================`n
 Please take note of the following ressource names, they will be used in the next steps `n
 ======================================================================================================
-            SQL Server name : $($sqlsvname).database.windows.net `n
             Azure Container Registry name : $($acrname).azurecr.io `n
+            SQL Server name : $($sqlsvname).database.windows.net `n
             Azure Kubernetes Services name : $($aksname) `n"
